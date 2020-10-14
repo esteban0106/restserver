7781eed5023dc8f7798545311c5554ad1cfba3b3
Esteban <esteban3971@gmail.com>
2020-10-14 15:34:39 -0500

    Conectar con mongo atlas

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/kanda';
 } else {
-    urlDB = process.env.MONGO_URL;
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
